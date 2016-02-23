9279483b68b968a8431fbd3e2aa570c24d743678
Aditya Balwani <Aditya Balwani>
2016-02-23 10:19:42 -0500

    Broken navbar, tbf

--- Frontend/templates/js/components/navigation/navbar.jsx
+++ Frontend/templates/js/components/navigation/navbar.jsx
@@ -1,14 +1,57 @@
 define([
     "jquery",
+
     "underscore",
     "react",
     "backbone"
 ], function($, _, React, backbone) {
     return React.createClass({
+        getInitialState: function(){
+            return {
+                'loggedIn': true
+            }
+        },
+        componentDidMount : function() {
+            $(".dropdown-button").dropdown();
+            $(".button-collapse").sideNav();
+        },
+        componentDidUpdate : function() {
+            $(".dropdown-button").dropdown();
+            $(".button-collapse").sideNav();
+        },
         render: function() {
+            if(this.state.loggedIn) {
+                var navItems = (<ul id="dropdown1" className="dropdown-content">
+                      <li><a href="#!">Profile</a></li>
+                      <li><a href="#!">Account Settings</a></li>
+                      <li><a href="#!">Logout</a></li>
+                    </ul>)
+                var mobileItems = (
+                    <ul className="side-nav" id="mobile-demo">
+                        <li><a href="#!">Profile</a></li>
+                        <li><a href="#!">Account Settings</a></li>
+                        <li><a href="#!">Logout</a></li>
+                    </ul>
+                )
+            } else {
+                var mobileItems = (<ul className="side-nav" id="mobile-demo">
+                        <li><a href="#!">Login</a></li>
+                    </ul>
+                )
+            }
             return(
                 <div>
-                    <h1>LOGO</h1>
+                    {navItems}
+                    <nav>
+                    <div className="white blue-grey-text text-darken-1 nav-wrapper">
+                        <a href="#" className="blue-grey-text text-darken-1 brand-logo">Logo</a>
+                        <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
+                            {!this.state.loggedIn ? <li><a href="#">Login</a></li> : <li>Welcome <a href="#">Name</a><li><a className="dropdown-button" href="#!" data-activates="dropdown1">Dropdown<i className="material-icons right">arrow_drop_down</i></a></li></li>}
+                        </ul>
+                        {mobileItems}
+                    </div>
+                    </nav>
                 </div>
             )
         }
