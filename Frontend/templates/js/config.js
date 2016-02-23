0ef37c8cdc3689ba57f55a958b1d878fb062fd22
Aditya Balwani <adibalwani@gmail.com>
2016-02-23 01:13:31 -0500

    Add all and commit

--- Frontend/templates/js/config.js
+++ Frontend/templates/js/config.js
@@ -1,11 +1,12 @@
 require.config({
 	baseUrl: "js/",
 	shim: {
-		"bootstrap" : {"deps" :['jquery']}
+		"materialize" : {"deps" :['jquery']}
 	},
  	paths: {
 	            "jquery": "bower_components/jquery/dist/jquery.min",
 	         "bootstrap": "bower_components/bootstrap/dist/js/bootstrap.min",
+		   "materialize": "bower_components/Materialize/dist/js/bootstrap.min",
 	              "flux": "bower_components/flux/dist/Flux",
 	             "react": "bower_components/react/react-with-addons",
 			 "react-dom": "bower_components/react/react-dom",
