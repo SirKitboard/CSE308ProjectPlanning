5756fb5141c5c0f4a1f5b6b092d2e55ef41e6a0a
Purav Shah <puravshah23@gmail.com>
2016-02-28 16:54:35 -0500

    Layout the search results page

--- Frontend/templates/js/components/base/searchresults.jsx
+++ Frontend/templates/js/components/base/searchresults.jsx
@@ -1,20 +1,79 @@
 define([
     'jquery',
     'underscore',
+    'react',
     'backbone',
-    'react'
-], function($, _, Backbone, React) {
+    'jsx!components/template/navbar',
+    'jsx!components/searchprofile/results'
+], function($, _, React, Backbone, NavigationBar, SearchResults) {
     return React.createClass({
         getInitialState : function() {
+            var book = {
+                title: null,
+                description: null,
+                author: null
+            };
+            books = []
+            for (i=0; i<16; i++) {
+                books.push(book)
+            }
             return {
-                'something' : true
+                'something' : true,
+                'results': books
             }
         },
+
+        componentDidMount: function() {
+            $('.dropdown-button').dropdown();
+        },
+
         render: function() {
             return(
                 <div>
                     <NavigationBar/>
-                    <h1>SEARCHES</h1>
+                    <div className="row searchBanner valign-wrapper"></div>
+                    <div className="row tab-row searchTabs z-depth-1">
+                        <div className="col s12">
+                            <div className="tab col s4 dropdown-button" href='#' data-activates='genreDropdown'>
+                                <p className="center-align">Genre</p>
+                            </div>
+                            <div className="tab col s4 dropdown-button" href='#' data-activates='titleDropdown'>
+                                <p className="center-align">Title</p>
+                            </div>
+                            <div className="tab col s4 dropdown-button" href='#' data-activates='authorDropdown'>
+                                <p className="center-align">Author</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row searchResults">
+                        <SearchResults books={this.state.results}/>
+                    </div>
+                    <div className="row">
+                        <div className="col s12">
+                            <div className="col s10 offset-s1">
+                                <button className="btn-large center-align" id="loadButton">Load More</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <ul id="genreDropdown" className='dropdown-content'>
+                       <li><a href="#!">one</a></li>
+                       <li><a href="#!">two</a></li>
+                       <li className="divider"></li>
+                       <li><a href="#!">three</a></li>
+                     </ul>
+
+                     <ul id="titleDropdown" className="dropdown-content">
+                         <form>
+                             <input type="text" />
+                         </form>
+                     </ul>
+
+                     <ul id="authorDropdown" className="dropdown-content">
+                         <form>
+                             <input type="text" />
+                         </form>
+                     </ul>
                 </div>
             )
         }
