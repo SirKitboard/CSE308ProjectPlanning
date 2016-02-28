3d202e7a180280abb8595c9ad838442a013abcf0
halaa menasy <menasyhalaa@gmail.com>
2016-02-28 15:39:19 -0500

    User profile stuff

--- Frontend/templates/js/components/base/user_profile.jsx
+++ Frontend/templates/js/components/base/user_profile.jsx
@@ -11,35 +11,63 @@ define([
 
             }
         },
+        componentDidMount : function() {
+            $('.collapsible').collapsible({
+              accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+            });
+        },
         render: function() {
-            return(
-                <div>
-                    <NavigationBar/>
-                    <div className="row">
-                        <div className="col s12 m7">
-                          <div className="card">
-                            <div className="card-image">
-                              <img src="http://placehold.it/200x200"/>
-                              <span className="card-title">Hello, User! </span>
+        var profileInfo = "";
+        var imageURL = "http://placehold.it/200x200"
+
+        profileInfo = (
+                    <div className="profile-and-stats row">
+                        <div className="col s12 m6 row profile">
+                           
+                            <div className="col s8 greeting">
+                            <img className="activator" src = {imageURL}/>
+                                <h4>firstName lastName</h4>
+                                <div className="button-rating">
+                                    <span className="amber-text text-lighten-2 rating">
+                                         <i className="material-icons">star</i>
+                                    </span>
+                                </div>
                             </div>
-                            <div className="card-content">
-                              <p>You are looking great today!</p>
-                            </div>
-                            <div className="card-action">
-                              <a className="waves-effect waves-light btn">Update Profile</a>
-                            </div>
-                          </div>
                         </div>
                         <div className="col s12 m6 right-align">
                             <h5>Stats</h5>
-                            You've borrowed <span className="bold green-text">Test</span><br/>
-                            You've reviwed <span className="bold green-text">test</span><br/>
-                            You've currently checked out <span className="bold green-text">Test</span><br/>
+                            You borrowed <span className="bold green-text"></span><br/>
+                            You Reviewed <span className="bold green-text"></span><br/>
+                            You Currently Checked out <span className="bold green-text"></span><br/>
+                            
                         </div>
-                      </div>
+                    </div>
+                )   
 
+        return (
+                <div className="container" style={{width:'90%', maxWidth:'none'}}>
+
+                    <div className="profile-card z-depth-1">
+                        {profileInfo}
+                    </div>
+                    <h5>User History</h5>
+                     <ul className="collapsible" data-collapsible="accordion">
+                        <li>
+                          <div className="collapsible-header"><i className="material-icons">lock</i>Books you have borrowed </div>
+                          <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                        </li>
+                        <li>
+                          <div className="collapsible-header"><i className="material-icons">rate_review</i>Books you have reviewed</div>
+                          <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                        </li>
+                        <li>
+                          <div className="collapsible-header"><i className="material-icons">library_books</i>Books you are currently enjoying</div>
+                          <div className="collapsible-body"><p>Lorem ipsum dolor sit amet.</p></div>
+                        </li>
+                    </ul>
+                       
                 </div>
-            )
+);
         }
     });
 })
