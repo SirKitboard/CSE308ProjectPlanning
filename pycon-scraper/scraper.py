2e15a7f726e6562210cda42f8b0af4eb3016de3c
Aditya Balwani <adibalwani@gmail.com>
2016-04-10 19:07:03 -0400

    Scraped 2 more books

--- pycon-scraper/scraper.py
+++ pycon-scraper/scraper.py
@@ -10,32 +10,34 @@ isbnsComplete = []
 
 baseURL = "https://openlibrary.org/api/books?bibkeys=%s&format=json&jscmd=data"
 
-start = 1000000000
-end = 1000100000
+start = 1000100000
+end   = 1000200000
 # end = 1000000100
 
 allISBNS = list(range(start, end))
 
 for isbn in allISBNS:
-    if(len(isbnsDict[-1]) == 10):
+    if(len(isbnsDict[-1]) == 20):
         isbnsDict.append([])
     isbnsDict[-1].append(isbn)
 
 # print(isbnsDict)
 
-pool = ThreadPool(8)
+pool = ThreadPool(16)
 
 def printFile():
+    print(len(isbns))
     with open('booksJSON.json', 'w') as outfile:
         json.dump(isbns, outfile)
 
 def getBook(inputISBN):
     urlString = []
     for isbn in inputISBN:
-        urlString.append("ISBN:%d" % isbn)
+        urlString.append("ISBN:%010d" % isbn)
         isbnsComplete.append(isbn)
 
     isbnURL = ','.join(urlString)
+    # print(isbnURL)
     r = requests.get(baseURL % isbnURL);
     print("%.3f%% %d" % ((len(isbnsComplete) * 100 / (end - start)), r.status_code))
     jsonBook = r.json()
